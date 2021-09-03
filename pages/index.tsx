e282208199ffaff5a0cd631f1a85147c269ac396
Pavel <recyclesordie94@gmail.com>
2021-09-03 19:00:49 +0300

    Добавил стили для ghost кнопки

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearance={"primary"}>Кнопка</Button>
+      <Button appearance={"ghost"}>Кнопка</Button>
     </>
   );
 }
