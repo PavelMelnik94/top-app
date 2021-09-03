ead8554a8299367603d62883c7fe02e3b0946258
Pavel <recyclesordie94@gmail.com>
2021-09-03 18:58:34 +0300

    компонент Button

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -1,4 +1,4 @@
-import { HtagProps } from "./HtagProps";
+import { HtagProps } from "./Htag.props";
 import styles from "./Htag.module.css";
 
 export const Htag = ({ tag, children }: HtagProps): JSX.Element => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
+      <Button appearance={"primary"}>Кнопка</Button>
     </>
   );
 }
